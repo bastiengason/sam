b7600cd7c6a0f81923b26a77562ec2dc08dff9f7
Anne Laure de Harlez <aldeharlez@gmail.com>
2016-12-06 17:26:11 +0100

    Profile show and edit added

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -1,2 +1,45 @@
 module ProfileHelper
+
+  def greet
+    now = Time.now
+    today = Date.today.to_time
+
+    morning = today.beginning_of_day
+    noon = today.noon
+    evening = today.change( hour: 19 )
+    night = today.change( hour: 22 )
+    tomorrow = today.tomorrow
+
+    if (morning..noon).cover? now
+      'Good Morning'
+    elsif (noon..evening).cover? now
+      'Good Afternoon'
+    elsif (evening..night).cover? now
+      'Good Evening'
+    elsif (night..tomorrow).cover? now
+      'Good Night'
+    end
+  end
+
+  def goodbye
+    now = Time.now
+    today = Date.today.to_time
+
+    morning = today.beginning_of_day
+    noon = today.noon
+    evening = today.change( hour: 19 )
+    night = today.change( hour: 22 )
+    tomorrow = today.tomorrow
+
+    if (morning..noon).cover? now
+      'Have a lovely day!'
+    elsif (noon..evening).cover? now
+      'Have a nice afternoon!'
+    elsif (evening..night).cover? now
+      'Have a wonderful evening!'
+    elsif (night..tomorrow).cover? now
+      'Have a restful night!'
+    end
+  end
+
 end
