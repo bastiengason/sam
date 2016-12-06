21192836c5ebc07cfced4373392d4576fb4ccc82
debora lasoen <dlasoen@gmail.com>
2016-12-06 14:39:10 +0100

    first tasks commit controller

--- config/routes.rb
+++ config/routes.rb
@@ -29,17 +29,18 @@ Rails.application.routes.draw do
 
   get 'tasks/index'
 
-  get 'tasks/new'
+  resources :tasks
+  # get 'tasks/new'
 
-  get 'tasks/create'
+  # get 'tasks/create'
 
-  get 'tasks/update'
+  # get 'tasks/update'
 
-  get 'tasks/edit'
+  # get 'tasks/edit'
 
-  get 'tasks/show'
+  # get 'tasks/show'
 
-  get 'tasks/destroy'
+  # get 'tasks/destroy'
 
   get 'categories/show'
 
