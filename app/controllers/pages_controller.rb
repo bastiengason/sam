60f1d34811c3f9c30a5a1c385c67a0ff2b359420
Jeddi Mees <jeddimees@gmail.com>
2016-12-07 11:08:19 +0100

    notif rdy

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,20 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    check_notif
+  end
+
+  private
+
+  def check_notif
+    @notif = false
+    if !current_user.nil?
+      current_user.tasks.each do |task|
+        task.notifications.each do |notification|
+          @notif = true if notification.status == "to_show"
+        end
+      end
+    end
+    @notif
   end
 end
