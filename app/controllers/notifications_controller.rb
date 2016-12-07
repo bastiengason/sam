37bf67c451b19fde0387bb865a63908513d0d2b9
Jeddi Mees <jeddimees@gmail.com>
2016-12-07 16:01:13 +0100

    finish notif methods + style

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -19,4 +19,25 @@ class NotificationsController < ApplicationController
 
   def destroy
   end
+
+  def notifdone
+
+ @notifications = current_user.notications
+      @notifications.each do |notification|
+
+         notification.status = "done" if notification.status == "to_show"
+      end
+      @notification.save!
+  end
+
+  def notifcancell
+ @notifications = current_user.notications
+      @notifications.each do |notification|
+
+         notification.status = "not done" if notification.status != "to_show"
+      end
+      @notification.save!
+
+  end
+
 end
