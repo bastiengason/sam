bc28d33302843ef49a583969ec67f2313806d60e
Anne Laure de Harlez <aldeharlez@gmail.com>
2016-12-08 12:19:56 +0100

    Stats work fine and I am proud of it

--- app/models/task.rb
+++ app/models/task.rb
@@ -9,12 +9,23 @@ class Task < ApplicationRecord
 
   def score_color
     if score < 30
-      "#50E3C2"
-    elsif score > 30 && score < 70
+      "#FC5252"
+    elsif score >= 30 && score < 70
       "#F5A623"
     else
       "#50E3C2"
     end
+  end
 
+  def motivation_sentence
+    if score < 30
+      "That's a good start!"
+    elsif score >= 30 && score < 70
+      "You're getting closer to make " + name.downcase + " an habit. Keep going! "
+    elsif score >= 70 && score < 100
+      "You're closed to make " + name.downcase + " an habit, good job!"
+    else
+      "Yay, you made " + name.downcase + " an habit. Well done!"
+    end
   end
 end
