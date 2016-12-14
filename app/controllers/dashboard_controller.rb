00584f8340503892a0c53e7761a06a80ee262c20
Bastien Gason <bastiengason@hotmail.com>
2016-12-14 23:05:34 +0100

    restyle herobanner-nav-greeting-signature

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,5 @@ class DashboardController < ApplicationController
   def category
     @tasks = current_user.tasks
     @category = Category.find(params[:id])
-    @category_name = task.find(params[:category_id])
   end
 end
